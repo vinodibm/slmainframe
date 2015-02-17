a8a789481dfbe5f2a0d69eb55c3ca613ec71e758
vinodibm <vinodibm@users.noreply.github.com>
2015-02-17 08:50:43 +0530

    Update batchtrigger.sh

--- batchtrigger.sh
+++ batchtrigger.sh
@@ -1,1 +1,14 @@
+route add -net 192.168.0.0 netmask 255.255.255.0 gw 10.108.1.83
+echo "//RAMALINA JOB ACCT, " > /root/tmpfile
+echo "// RAMALIN, " >> /root/tmpfile
+echo "// CLASS=A, " >> /root/tmpfile
+echo "// MSGCLASS=Q, " >> /root/tmpfile
+echo "// NOTIFY=&SYSUID," >> /root/tmpfile
+echo "// REGION=0M " >> /root/tmpfile
+echo "//STEP1 EXEC PGM=IKJEFT01 " >> /root/tmpfile
+echo "//SYSTSPRT DD SYSOUT=A " >> /root/tmpfile
+echo "//SYSTSIN DD * " >> /root/tmpfile
+echo " LISTUSER " >> /root/tmpfile
+echo "/* " >> /root/tmpfile
+curl -k -i -X PUT https://ramalin:thinking@ZOSBIGDA.CTL.LOCAL:32208/zosmf/restjobs/jobs -H "Content-Type: text/plain" -H "X-IBM-Intrdr-Class: A" --data-binary @/root/tmpfile >> /root/startup
 date >> /root/startup
